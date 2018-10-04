72c48464f39032887806e3db8a671057c4b14ad1
Cr0w Tom <serbinistom007@gmail.com>
2018-10-04 13:35:25 +0300

    Initialization of files

---
+++ Patches/readEC.cmd
@@ -0,0 +1,22 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges... 
+goto request
+) else (goto start)
+
+:request
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+set params = %*:"=""
+echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /b
+
+:start
+pushd %~dp0bin
+echo Xiaomi Mi Notebook Pro EC firmware reader by _Cyb_
+echo Tested on 0603 BIOS only
+fptw -d ../ec.bin -ADDRESS 2097152 -LENGTH 131072
+
+pause
